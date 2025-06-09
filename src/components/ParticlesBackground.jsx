debd8e7eb544398d658894e22097640e5f4163e7
KronosCoder <atasit,.saengsak@gmail.com>
2025-06-09 16:07:25 +0700

    hahaha

---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './ParticlesBackground.css'
+
+export default function () {
+  return (
+    <div>
+      <div className="particle-container">
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+        <div className="particles"></div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,112 +1,112 @@
-  import { useState , useEffect } from 'react'
-  import './Home.css'
-  import PasswordGenerator from '../../utils/PasswordGenerator'
+import { useState, useEffect } from 'react';
+import './Home.css';
+import PasswordGenerator from '../../utils/PasswordGenerator';
+import ParticlesBackground from '../../components/ParticlesBackground';
 
-  function Home () {  
-    const [options , setOptions] = useState([])
-    const [pwdLength , setPwdLength] = useState(0)
-    const [pwdGenerated , setPwdGenerated] = useState('') 
-    const [loading , setLoading] = useState(false)
 
-    const handleOptions = (optionName) => {
-      setOptions((prevOptions) => {
-        return prevOptions.includes(optionName)  
-        ? prevOptions.filter(opt => opt !== optionName)
-        : [...prevOptions , optionName]
-      })
-    }
+function Home() {
+  const [options, setOptions] = useState([]);
+  const [pwdLength, setPwdLength] = useState(0);
+  const [pwdGenerated, setPwdGenerated] = useState('');
+  const [loading, setLoading] = useState(false);
+  const optionList = ['numbers', 'symbols', 'uppercase' , 'lowercase'];
 
-    const handleLength = (e) => {
-      const val = Number(e.target.value);
-      setPwdLength(val)
-    }
+  const handleOptions = (optionName) => {
+    setOptions((prevOptions) => {
+      return prevOptions.includes(optionName)
+        ? prevOptions.filter((opt) => opt !== optionName)
+        : [...prevOptions, optionName];
+    });
+  };
 
-    const getPasswordGenerated = async () => {
-      setLoading(true); 
-      const pwd = await PasswordGenerator(options, pwdLength);
-      setPwdGenerated(pwd);
-      setLoading(false);
-    }
+  const handleLength = (e) => {
+    const val = Number(e.target.value);
+    setPwdLength(val);
+  };
+
+  const getPasswordGenerated = async () => {
+    setLoading(true);
+    const pwd = await PasswordGenerator(options, pwdLength);
+    setPwdGenerated(pwd);
+    setLoading(false);
+  };
 
   return (
-    <div className='home-screen relative h-screen bg-neutral-900 flex justify-center items-center overflow-hidden'>
-        <div className="particle-shape"></div>
-        <div className="particle-shape"></div>
-        <div className="w-[80%] lg:w-[50%] xl:w-[40%] h-auto bg-white-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-30 border border-gray-100 p-8">
-          <h1 className='text-white text-3xl font-bold text-center mb-8'>Password Generator</h1>
-          <div className="w-full grid grid-cols-4 gap-2 ">
-               <input type="number" 
-                className='w-full bg-transparent py-3 px-4 rounded-md text-white font-light border border-white outline-none placeholder-white text-xs lg:text-sm col-span-3' 
-                placeholder='Fill your password length here ...' 
-                pattern='\d+' 
-                title='Please input only number.' 
-                onChange={handleLength}
-              />
-              <button 
-                className={`col-span-1 outline-none text-black font-bold transition-colors bg-white rounded-md ${loading ? 'loading' : 'hover:bg-gray-400'}`} 
-                onClick={getPasswordGenerated}
-                disabled={loading}
-              >
-                {loading ? (
-                  <div className="loader"></div>
-                ) : (
-                  'Generate'
-                )}
-              </button>
+    <>
+      {/* Particles Background Component */}
+      {/* Main Content */}
+      <div className="home-screen relative h-screen flex justify-center items-center overflow-hidden z-10"
+          style={{ backgroundImage: 'linear-gradient(180deg, black 40%, rgba(255 , 255 , 255 , 0.195))' }}
+      >
+        <ParticlesBackground />
+        <div className="w-[80%] lg:w-[50%] xl:w-[40%] h-auto bg-white-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-white p-8 box" 
+          style={{ boxShadow: '0 0 8px #fff' }}
+        >
+          <h1 className="text-white text-3xl font-bold text-center mb-8">
+            Password Generator
+          </h1>
+          <div className="w-full grid grid-cols-4 gap-2">
+            <input
+              type="number"
+              className="w-full bg-transparent py-3 px-4 rounded-md text-white font-light border border-white outline-none placeholder-white text-xs lg:text-sm col-span-3"
+              placeholder="Fill your password length here ..."
+              pattern="\d+"
+              title="Please input only number."
+              onChange={handleLength}
+            />
+            <button
+              className={`col-span-1 outline-none text-white font-bold transition-colors bg-transparent border border-white rounded-md ${
+                loading ? 'loading' : 'hover:bg-white hover:text-black'
+              }`}
+              onClick={getPasswordGenerated}
+              disabled={loading}
+            >
+              {loading ? <div className="loader"></div> : 'Generate'}
+            </button>
           </div>
-          <form action="" className='mt-4 w-full'>
-            <fieldset className='border border-white p-4 rounded-md'>
-              <legend className='text-white font-semibold'>Options</legend>  
-              <div className='flex justify-between gap-3'>
-                <div className="">
-                  <input 
-                    type="checkbox" 
-                    name="" 
-                    id="numbers_check" 
-                    className="hidden"
-                    onChange={() => handleOptions('numbers')}
-                  />
-                  <label 
-                    htmlFor="numbers_check" 
-                    className='option-buttons'
-                  >Numbers</label>
-                </div>
-                <div className="">
-                  <input 
-                    type="checkbox" 
-                    name="" 
-                    id="lowercase_check" 
-                    className="hidden"
-                    onChange={() => handleOptions('lowercase')} 
-                  />
-                  <label htmlFor="lowercase_check" className='option-buttons'>Lowercase</label>
-                </div>
-                <div className="">
-                  <input 
-                    type="checkbox" 
-                    name="" 
-                    id="uppercase_check" 
-                    className="hidden"
-                    onChange={() => handleOptions('uppercase')} 
-                  />
-                  <label htmlFor="uppercase_check" className='option-buttons'>Uppercase</label>
-                </div>
-                <div className="">
-                  <input 
-                    type="checkbox" 
-                    name="" 
-                    id="symbols_check" 
-                    className="hidden"
-                    onChange={() => handleOptions('symbols')} 
-                  />
-                  <label htmlFor="symbols_check" className='option-buttons'>Symbols</label>
-                </div>
+
+          <form action="" className="mt-4 w-full">
+            <fieldset className="border border-white p-4 rounded-md">
+              <legend className="text-white font-semibold">Options</legend>
+              <div className="flex justify-between gap-3">
+                {optionList.map((option) => (
+                  <div key={option}>
+                    <input
+                      type="checkbox"
+                      id={`${option}_check`}
+                      className="hidden"
+                      onChange={() => handleOptions(option)}
+                    />
+                    <label
+                      htmlFor={`${option}_check`}
+                      className={`option-buttons ${options.includes(option) ? 'opt-activce' : ''}`}
+                    >
+                      {option.charAt(0).toUpperCase() + option.slice(1)}
+                    </label>
+                  </div>
+                ))}
               </div>
-            </fieldset>    
+            </fieldset>
           </form>
+
+          <div className="w-full mt-4 flex">
+              <div className="w-[90%] h-[40px] bg-slate-100 rounded-l-md overflow-hidden p-2">
+               <input 
+                type="text" 
+                className='w-full h-full outline-none px-4'
+                name="" 
+                id="" 
+                placeholder='Generated Password Here ...'
+                disabled={true}
+               /> 
+              </div>
+              <div className="flex-1 h-[40px] bg-gray-300 rounded-r-md transition-colors cursor-pointer hover:bg-gray-400"></div>
+          </div>
+          
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
